c240fdbb0e322f3c9ce5a133a0d6ff1e3b5b6df3
Patricio <plooferman@gmail.com>
2020-05-14 18:07:59 -0300

    Agregado Fragment Noticias Barriales

---
+++ app/src/main/java/kalitero/software/noticiasargentinas/NoticiasBarrialesFragment.java
@@ -0,0 +1,28 @@
+package kalitero.software.noticiasargentinas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NoticiasBarrialesFragment extends Fragment {
+
+    public NoticiasBarrialesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_noticias_barriales, container, false);
+    }
+}
